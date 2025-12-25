9fbd8b3087b8d890b3103c1585ed1c9752fa1869
Fatemeh <fa.moddarres2004@gmail.com>
2025-12-25 13:28:50 +0330

    add row function

--- app/task_managment.py
+++ app/task_managment.py
@@ -1,14 +1,16 @@
-from typing import Optional, List
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
 import sqlite3
 
-
 database_file = "todo.db"
-def init_database():
+
+def get_connection():
     connection = sqlite3.connect(database_file)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def init_database():
+    connection = get_connection()
     connection.execute("""
-        CREATE TABLE IF NOT EXISTS TASKS(
+        CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             description TEXT,
@@ -23,3 +25,18 @@ def init_database():
     """)
     connection.commit()
     connection.close()
+
+def row_to_dict(row):
+    return {
+        "id": row["id"],
+        "title": row["title"],
+        "description": row["description"],
+        "is_done": bool(row["is_done"]),
+        "created_at": row["created_at"],
+        "updated_at": row["updated_at"],
+        "starts": row["starts"],
+        "due_date": row["due_date"],
+        "level": row["level"],
+        "priority": row["priority"],
+    }
+
